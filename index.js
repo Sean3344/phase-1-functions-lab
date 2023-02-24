62895aa08d4d0fcf21367f585c334f45c0ea2c4f
Sean Nekouei <sean.nekouei@gmail.com>
2023-02-24 11:03:44 -0700

    commit tree ca4dfc06b224cc08cce080236fa768e24d8f4b9a

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Code your solution in this file!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function distanceFromHqInBlocks(start){
+    return Math.abs(start-42)
+}
+
+
+
+
+function distanceFromHqInFeet(start) {
+    return Math.abs((start-42)*264)
+    
+}
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start-end)*264)
+}
+function calculatesFarePrice(start, destination) {
+    const totalDistance = distanceTravelledInFeet(start, destination)
+    if (totalDistance <=400) {
+        return 0
+    }
+
+    else if (totalDistance>400 && totalDistance <=2000) {
+        return (totalDistance-400)*0.02
+    }
+    else if (totalDistance>2000 && totalDistance <=2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
